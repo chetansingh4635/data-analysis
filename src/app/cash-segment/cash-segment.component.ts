927fa776d2b9ced346d4c696554649a680683400
Chetan Singh <chetansingh127@gmail.com>
2022-10-17 23:50:58 +0530

    Changed options view

--- src/app/cash-segment/cash-segment.component.ts
+++ src/app/cash-segment/cash-segment.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { CashSegmentService } from './cash-segment.service';
 import url from '../configs/config.json';
 
@@ -8,22 +8,39 @@ import url from '../configs/config.json';
   styleUrls: ['./cash-segment.component.css']
 })
 
-export class CashSegmentComponent implements OnInit {
+export class CashSegmentComponent implements OnInit, OnDestroy {
 
   public nifty_stocks_data: any;
+  public advance_to_decline: any;
+  public nifty_data: any;
+  public interval_instance: any;
+
   constructor(private service: CashSegmentService) {
-    service.getNiftyStocksData().subscribe((data:any) => {
-      this.nifty_stocks_data = data.data ? data : { data : []};
+    service.getNiftyStocksData().subscribe((data: any) => {
+      this.nifty_stocks_data = data.data ? data : { data: [] };
+      this.advance_to_decline = data.advance ? data.advance : {};
+      this.nifty_data = data.metadata ? data.metadata : {}
     });
-  //   fetch(url.nifty_options_live_data_previous)
-  // .then((response) => response.json())
-  // .then((data) => console.log(data));
   }
 
   ngOnInit(): void {
+    this.interval_instance = setInterval(() => {
+      this.updateNiftyDataOnInterval();
+    }, 1000);
   }
 
-  public niftyData() {
+  public updateNiftyDataOnInterval() {
+    this.service.getNiftyStocksData().subscribe((data: any) => {
+      this.nifty_stocks_data = data.data ? data : { data: [] };
+      this.advance_to_decline = data.advance ? data.advance : {};
+      this.nifty_data = data.metadata ? data.metadata : {}
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.interval_instance) {
+      clearInterval(this.interval_instance);
+    }
   }
 
 }
--- src/app/options-segment/options-segment.component.ts
+++ src/app/options-segment/options-segment.component.ts
@@ -15,6 +15,9 @@ export class OptionsSegmentComponent implements OnInit, OnDestroy {
   public current_nifty_value: any = 0;
   public mostly_traded_strikes: number[] = [];
   public options_data: any = [];
+  public selected_strike_data: any = { data: [] };
+  public interval_instance: any;
+
   constructor(private route: ActivatedRoute, private service: OptionsSegmentService) {
 
     this.symbol = this.route.snapshot.paramMap.get('symbol');
@@ -23,7 +26,9 @@ export class OptionsSegmentComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit(): void {
-
+    this.interval_instance = setInterval(() => {
+      this.getMostActiveStrikesandSetData();
+    }, 1000 * 60);
   }
 
   public getMostActiveStrikesandSetData() {
@@ -43,13 +48,14 @@ export class OptionsSegmentComponent implements OnInit, OnDestroy {
         });
         const index = strike_prices.indexOf(closestStrikePrice);
         this.mostly_traded_strikes = [strike_prices[index], strike_prices[index - 1], strike_prices[index - 2], strike_prices[index - 3],
-        strike_prices[index + 1], strike_prices[index + 2], strike_prices[index + 3]]
+        strike_prices[index + 1], strike_prices[index + 2], strike_prices[index + 3]];
+
         this.nifty_options_data = data?.filtered?.data ? data.filtered.data.filter((strikes: any) => this.mostly_traded_strikes.includes(strikes.strikePrice)) : { data: [] };
         if (localStorage.getItem('options_data')) {
           this.options_data = JSON.parse(localStorage.getItem('options_data') || "[]");
           this.options_data.forEach((strike_data: any) => {
             let obj = this.nifty_options_data.find((data: any) => data.strikePrice == strike_data.strike);
-            obj.time = new Date().toLocaleTimeString();;
+            obj.time = new Date().toLocaleTimeString();
             strike_data.data = strike_data.data.concat([{ ...obj }]);
           })
           localStorage.setItem('options_data', JSON.stringify(this.options_data))
@@ -63,14 +69,28 @@ export class OptionsSegmentComponent implements OnInit, OnDestroy {
           localStorage.setItem('options_data', JSON.stringify(this.options_data))
         }
         this.options_data = JSON.parse(localStorage.getItem('options_data') || "[]");
-console.log('options data', this.options_data);
+        if (this.selected_strike_data.strike)
+          this.selectedStrikePrice(this.selected_strike_data.strike);
+        console.log('options data', this.options_data);
 
       });
     });
   }
 
-  ngOnDestroy(): void {
+  selectedStrikePrice(strike: number) {
+    this.selected_strike_data = this.options_data.filter((data: any) => data.strike === strike)[0];
+    console.log(this.selected_strike_data.data.sort((a: any, b: any) => b.time - a.time));
 
+    this.selected_strike_data.data.reverse();
+
+    console.log('selected_strike_data', this.selected_strike_data);
+
+  }
+
+  ngOnDestroy(): void {
+    if (this.interval_instance) {
+      clearInterval(this.interval_instance);
+    }
   }
 
 }
